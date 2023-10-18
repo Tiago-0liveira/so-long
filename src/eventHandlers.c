5f5045db0aba1cb31533a512756d3a37322128d6
Tiago Filipe Oliveira <tiagoliv@c2r4s6.42lisboa.com>
2023-10-18 17:17:54 +0100

    fixed event handler for x

--- src/eventHandlers.c
+++ src/eventHandlers.c
@@ -6,33 +6,40 @@
 /*   By: tiagoliv <tiagoliv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/06 14:44:03 by tiagoliv          #+#    #+#             */
-/*   Updated: 2023/10/09 15:46:15 by tiagoliv         ###   ########.fr       */
+/*   Updated: 2023/10/18 16:23:33 by tiagoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/so_long.h"
 
-static  int win_close(int keycode, win_t *win);
-static  int key_press(int keycode, win_t *win);
+static int	key_press(int keycode, t_win *win);
+static int	close_win(t_win *win);
 
-void    init_event_handlers(win_t *win)
+void	init_event_handlers(t_win *win)
 {
-    mlx_hook(win->win, DestroyNotif, NoEventMask, win_close, win);
     mlx_hook(win->win, KeyPress, KeyPressMask, key_press, win);
-    mlx_loop(win->mlx);
+    mlx_hook(win->win, DestroyNotify, KeyPressMask, close_win, win);
 }
 
-static int win_close(int keycode, win_t *win)
+static int	close_win(t_win *win)
 {
-    (void)keycode;
-    mlx_destroy_window(win->mlx, win->win);
-    mlx_destroy_display(win->mlx);
-    free(win->mlx);
+    if (win)
+    {
+        if (win->win)
+            mlx_destroy_window(win->mlx, win->win);
+        if (win->mlx)
+        {
+            mlx_destroy_display(win->mlx);
+            free(win->mlx);
+        }
+        free(win);
+    }
     exit(0);
 }
 
-static  int key_press(int keycode, win_t *win)
+static int	key_press(int keycode, t_win *win)
 {
+    ft_printf("key_press\n");
     if (keycode == W || keycode == UP)
         ft_printf("up");
     else if (keycode == A || keycode == LEFT)
@@ -42,6 +49,10 @@ static  int key_press(int keycode, win_t *win)
     else if (keycode == D || keycode == RIGHT)
         ft_printf("right");
     else if (keycode == ESC)
-        win_close(keycode, win);
+        close_win(win);
+    else
+    {
+        ft_printf("debug|keycode:%d\n", keycode);
+    }
     return (0);
 }
